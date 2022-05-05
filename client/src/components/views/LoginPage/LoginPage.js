f41399d8c6d3534cf45e94734d18f17c4513d3be
HyunJay52 <hyunjungk52@gmail.com>
2022-05-06 03:16:24 +0900

    working on login router error

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,70 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+// import axios from "axios";
+import React, { useState } from "react";
+import { loginUser } from '../../../_actions/user_action'
 
-function LoginPage( ) {
+import { useNavigate } from 'react-router-dom'
+
+function LoginPage(props) {
+    // redux dispatch 추가
+    const dispatch = useDispatch();
+
+    // state 추가 -> 처음 빈칸
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    // state에 change 반영
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const onSubmithandler = (event) => {
+        event.preventDefault();
+
+        console.log("email- > ", Email);
+        console.log("password -> ",Password);
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        let history = useNavigate();
+        // redux dispatch를 이용해 action을 취함
+        // 아래 request 처리 부분을 redux에서 실행함 -> user_action.js
+        // axios.post('/api/users/login', body).then(response => {response.data});
+        // loginUser === action name
+        dispatch(loginUser(body))
+        .then(response => {
+            //로그인 성공시 랜딩페이지로 이동
+            if(response.payload.loginSuccess){
+                //props.first.history.push('/')
+                history('/')
+            }else{
+                alert('Error')
+                console.log(response)
+            }
+        })
+        // action > reducer > store > subscribe > react component > dispatch(action) > action 
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display:'flex', flexDirection: 'column'}}
+                onSubmit={onSubmithandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}/>
+                <br/>
+                <button type="submit">Login</button>
+            </form>    
         </div>
     )
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,83 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { registerUser } from '../../../_actions/user_action'
 
-function RegisterPage( ) {
+import { useNavigate } from 'react-router-dom';
+
+function RegisterPage(props) {
+    // redux dispatch 추가
+    const dispatch = useDispatch();
+
+    // state 추가 -> 처음 빈칸
+    const [Email, setEmail] = useState("")
+    const [Name, setName] = useState("")
+    const [Password, setPassword] = useState("")
+    const [ConfirmPassword, setConfirmPassword] = useState("")
+
+    // state에 change 반영
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    }
+    const onNameHandler = (event) => {
+        setName(event.currentTarget.value);
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+    const onConfirmEmailHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value);
+    }
+
+    let history = useNavigate();
+    
+    const onSubmithandler = (event) => {
+        event.preventDefault();
+
+        console.log("email- > ", Email);
+        console.log("password -> ",Password);
+
+        if(Password !== ConfirmPassword) {
+            return alert("비밀번호가 동일하지 않습니다.");
+        }
+
+        let body = {
+            email: Email,
+            name: Name,
+            password: Password
+        }
+
+        // redux dispatch를 이용해 action을 취함
+        dispatch(registerUser(body))
+        .then(response => {
+            if(response.payload.success){
+                //props.history.push('/login');
+                history('/');
+            }else{
+                alert('Error')
+                console.log(response)
+            }
+        })
+    }
+
+
     return (
-        <div>
-            RegisterPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display:'flex', flexDirection: 'column'}}
+                onSubmit={onSubmithandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Name</label>
+                <input type="text" value={Name} onChange={onNameHandler}/>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}/>
+                <label>Confirm Password</label>
+                <input type="password" value={ConfirmPassword} onChange={onConfirmEmailHandler}/>
+                <br/>
+                <button type="submit">Sign up</button>
+            </form>    
         </div>
     )
 }
